4c491ba11fb7d9eab79d23cf2f68e1344ec4f14d
Ilia_Kukhnavets <ilya_kukhnavets@bitnet.by>
2022-06-25 01:48:34 +0300

    add base fragment

--- app/src/main/java/com/example/bookapp/MainActivity.kt
+++ app/src/main/java/com/example/bookapp/MainActivity.kt
@@ -1,4 +1,20 @@
 package com.example.bookapp
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun initView() {
+
+    }
+
+    private fun initViewModel() {
+
+    }
 }
