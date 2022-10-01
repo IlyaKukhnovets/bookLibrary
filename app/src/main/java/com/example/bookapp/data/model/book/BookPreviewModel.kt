d7bdf9775f51041346fdd1080adf15c3d97546db
Ilia_Kukhnavets <ilya_kukhnavets@bitnet.by>
2022-10-01 03:02:51 +0300

    add new ui

--- app/src/main/java/com/example/bookapp/data/model/book/BookPreviewModel.kt
+++ app/src/main/java/com/example/bookapp/data/model/book/BookPreviewModel.kt
@@ -3,7 +3,7 @@ package com.example.bookapp.data.model.book
 data class BookPreviewModel(
     val name: String,
     val author: String,
-    val image: String?,
+    val image: String,
     val pagesCount: Int?,
     val bookDescription: String?,
     val status: Int,
--- app/src/main/java/com/example/bookapp/presentation/viewstate/book/BookPreviewViewState.kt
+++ app/src/main/java/com/example/bookapp/presentation/viewstate/book/BookPreviewViewState.kt
@@ -5,7 +5,7 @@ import com.example.bookapp.presentation.viewstate.home.BookStatus
 data class BookPreviewViewState(
     val bookName: String,
     val author: String,
-    val image: String?,
+    val image: String,
     val pagesCount: Int,
     val bookDescription: String?,
     val status: BookStatus,
