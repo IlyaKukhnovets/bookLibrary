b251561d35162914f4c157536cf25945dfec6b14
Ilia_Kukhnavets <ilya_kukhnavets@bitnet.by>
2023-01-18 01:36:07 +0300

    bottom navigation iteration

---
+++ app/src/main/java/com/example/bookapp/presentation/ui/info/BooksInfoFragment.kt
@@ -0,0 +1,8 @@
+package com.example.bookapp.presentation.ui.info
+
+import com.example.bookapp.R
+import com.example.bookapp.presentation.base.BaseFragment
+
+class BooksInfoFragment : BaseFragment(R.layout.fragment_books_info) {
+
+}
